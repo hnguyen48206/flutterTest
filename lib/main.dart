44dee3244a35ed585848901222032289aaf16f50
hnguyen48206 <hoangnt.it2@vnpt.vn>
2021-05-24 16:40:35 +0700

    pass co ban phan 1 ve widget stateful / stateless

--- lib/main.dart
+++ lib/main.dart
@@ -27,20 +27,35 @@ class TestWidget extends StatefulWidget {
 }
 
 class _TestWidgetState extends State<TestWidget> {
+  final _suggestions = <WordPair>[];
+  final _biggerFont = const TextStyle(fontSize: 18);
+
   @override
   Widget build(BuildContext context) {
     //return single value
     // final wordPair = WordPair.random();
     // return Text(wordPair.asPascalCase);
 
-    //return list value
-    final _suggestions = <WordPair>[];
-    final _biggerFont = const TextStyle(fontSize: 18);
+    //return a list instead
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Startup Name Generator'),
+      ),
+      body: _buildSuggestions(),
+    );
   }
 
+  // ignore: unused_element
   Widget _buildSuggestions() {
     return ListView.builder(
         padding: const EdgeInsets.all(16),
+        // The itemBuilder callback is called once per suggested
+        // word pairing, and places each suggestion into a ListTile
+        // row. For even rows, the function adds a ListTile row for
+        // the word pairing. For odd rows, the function adds a
+        // Divider widget to visually separate the entries. Note that
+        // the divider may be difficult to see on smaller devices.
+
         itemBuilder: (BuildContext _context, int i) {
           // Add a one-pixel-high divider widget before each row
           // in the ListView.
@@ -58,5 +73,14 @@ class _TestWidgetState extends State<TestWidget> {
           }
           return _buildRow(_suggestions[index]);
         });
+  }
+
+  Widget _buildRow(WordPair pair) {
+    return ListTile(
+      title: Text(
+        pair.asPascalCase,
+        style: _biggerFont,
+      ),
+    );
   }
 }
